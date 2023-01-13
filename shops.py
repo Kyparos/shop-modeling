f0547c328ca86e0552cf549b5b77222fbb04b413
Kyparos <sasha.malakhatka@gmail.com>
2023-01-13 01:01:54 +0100

    Implemented Store.run()

--- shops.py
+++ shops.py
@@ -3,6 +3,8 @@ from simpy import Environment
 from products import Product
 from stocks import Stockpile
 from settings import SHOP_DEFAULT_RNG
+from processes import customer_generator
+import logging
 
 rng = np.random.default_rng(SHOP_DEFAULT_RNG)
 
@@ -11,11 +13,24 @@ class Shop:
     def __init__(self,
                  env: Environment,
                  stockpiles: Stockpile,
-                 customer_generator=None):  # TODO: implement customer generator
-        self.customer_generator = customer_generator
+                 work_hours=(8, 20),
+                 cgenerator=customer_generator()):
+        self.work_hours = work_hours
+        self.customer_generator = cgenerator
         self.env = env
         self.stockpiles = stockpiles
-        self.action = env.process(self.run(env))
+        self.action = env.process(self.run())
+        logging.info('Shop created')
 
-    def run(self, env):
-        pass  # TODO: implement run
+    def run(self):
+        while True:
+            if self.work_hours[0] <= self.env.now < self.work_hours[1]:
+                yield self.env.process(self.sell())
+            else:
+                yield self.env.process(self.sell())
+
+    def sell(self):
+        pass
+
+    def closed(self):
+        pass
